e0019bb1283515b3ed71d82a0e8af5d7ac479600
El Chivo <gbaque@nelson.local>
2025-04-03 13:41:07 +0200

    Eliminacions de archivos

--- script2.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const result = document.querySelector('.result');
-const error = document.querySelector('.error');
-
-document.addEventListener('DOMContentLoaded', () => {
-    form.addEventListenerv('submit', (event) => {
